4505b7e435c7a5a906fabfe8c4e96449b98556e9
신우 김 <sinsdhk@gmail.com>
2017-12-28 17:50:38 +0900

    이미지 ./추가

--- 팀포폴/mainGame_images.cpp
+++ 팀포폴/mainGame_images.cpp
@@ -17,34 +17,34 @@ void mainGame::setImages()
 	IMAGEMANAGER->addFrameImage("main_video", "./image/UI/OPTION/main_video.bmp", 486, 55, 3, 1, true, RGB(255, 0, 255));
 	
 	//2. OPTION_CONTROLS
-	IMAGEMANAGER->addImage("option_controls_background", "/image/UI/OPTION/option_controls.bmp", 821, 514, false, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("controls_editController", "/image/UI/OPTION/controls_editcontroller.bmp", 1246, 56,3,1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("controls_editKeyBoard", "/image/UI/OPTION/controls_editkeyboard.bmp", 1101, 56, 3, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("controls_relicInputType", "/image/UI/OPTION/controls_relicinputtype.bmp", 1332, 55, 3, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("controls_relicInputType_options", "/image/UI/OPTION/controls_relicinputtype_select.bmp", 460, 55, 2, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("option_controls_background", "./image/UI/OPTION/option_controls.bmp", 821, 514, false, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("controls_editController", "./image/UI/OPTION/controls_editcontroller.bmp", 1246, 56,3,1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("controls_editKeyBoard", "./image/UI/OPTION/controls_editkeyboard.bmp", 1101, 56, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("controls_relicInputType", "./image/UI/OPTION/controls_relicinputtype.bmp", 1332, 55, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("controls_relicInputType_options", "./image/UI/OPTION/controls_relicinputtype_select.bmp", 460, 55, 2, 1, true, RGB(255, 0, 255));
 
 	//3. OPTION_GAME
-	IMAGEMANAGER->addImage("option_game_background", "/image/UI/OPTION/option_game.bmp", 821, 514, false, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("game_pauseOnLostFocus", "/image/UI/OPTION/game_pauseonlostfocus.bmp", 1563, 56, 3, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("game_showFeats", "/image/UI/OPTION/game_showfeats.bmp", 870, 56, 3, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("enable_option", "/image/UI/OPTION/select_enable.bmp", 406, 24, 2, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("option_game_background", "./image/UI/OPTION/option_game.bmp", 821, 514, false, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("game_pauseOnLostFocus", "./image/UI/OPTION/game_pauseonlostfocus.bmp", 1563, 56, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("game_showFeats", "./image/UI/OPTION/game_showfeats.bmp", 870, 56, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("enable_option", "./image/UI/OPTION/select_enable.bmp", 406, 24, 2, 1, true, RGB(255, 0, 255));
 
 	//4. OPTION_AUDIO
-	IMAGEMANAGER->addImage("option_audio_background", "/image/UI/OPTION/option_audio.bmp", 822, 514, false, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("audio_musicVolume", "/image/UI/OPTION/audio_musicvolume.bmp", 1023, 56, 3, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("audio_soundVolume", "/image/UI/OPTION/audio_soundvolume.bmp", 1023, 56, 3, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("audio_setToDefault", "/image/UI/OPTION/audio_settodefault.bmp", 1176, 55, 3, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addImage("audio_progressBar", "/image/UI/OPTION/audio_progressBar.bmp", 155, 22, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addImage("audio_progressValue", "/image/UI/OPTION/audio_progressBar_value.bmp", 9, 21, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("option_audio_background", "./image/UI/OPTION/option_audio.bmp", 822, 514, false, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("audio_musicVolume", "./image/UI/OPTION/audio_musicvolume.bmp", 1023, 56, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("audio_soundVolume", "./image/UI/OPTION/audio_soundvolume.bmp", 1023, 56, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("audio_setToDefault", "./image/UI/OPTION/audio_settodefault.bmp", 1176, 55, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("audio_progressBar", "./image/UI/OPTION/audio_progressBar.bmp", 155, 22, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("audio_progressValue", "./image/UI/OPTION/audio_progressBar_value.bmp", 9, 21, true, RGB(255, 0, 255));
 
 	//5. OPTION_VIDEO
-	IMAGEMANAGER->addImage("option_video_background", "/image/UI/OPTION/option_video.bmp", 823, 514, false, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("video_brightness", "/image/UI/OPTION/video_brightness.bmp", 870, 56, 3, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("video_fullScreen", "/image/UI/OPTION/video_fullscreen.bmp", 870, 56, 3, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("video_resulution", "/image/UI/OPTION/video_resulution.bmp", 870, 55, 3, 1, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addFrameImage("video_screenScale", "/image/UI/OPTION/video_screenscale.bmp", 1023, 56, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("option_video_background", "./image/UI/OPTION/option_video.bmp", 823, 514, false, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("video_brightness", "./image/UI/OPTION/video_brightness.bmp", 870, 56, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("video_fullScreen", "./image/UI/OPTION/video_fullscreen.bmp", 870, 56, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("video_resulution", "./image/UI/OPTION/video_resulution.bmp", 870, 55, 3, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("video_screenScale", "./image/UI/OPTION/video_screenscale.bmp", 1023, 56, 3, 1, true, RGB(255, 0, 255));
 
-	IMAGEMANAGER->addFrameImage("OnOff", "/image/UI/OPTION/on_off.bmp", 148, 56, 2, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("OnOff", "./image/UI/OPTION/on_off.bmp", 148, 56, 2, 1, true, RGB(255, 0, 255));
 
 	//아이템 이미지
 	//인벤 이미지들.
