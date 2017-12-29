b07ecf261707eb831b5bdbc851ecaf85856185b3
신우 김 <sinsdhk@gmail.com>
2017-12-29 13:51:45 +0900

    시스템옵션 추가 171229 1351i

--- 팀포폴/system_option.cpp
+++ 팀포폴/system_option.cpp
@@ -34,7 +34,53 @@ void system_option::update()
 
 	if (KEYMANAGER->isOnceKeyDown(VK_RETURN))
 	{
-
+		switch (_currentOption->getConnectedOption())
+		{
+				/*OPTION_OUT,
+				OPTION_MAIN,
+				OPTION_GAME,
+				OPTION_CONTROLS,
+				OPTION_AUDIO,
+				OPTION_VIDEO,
+				OPTION_SELECT,
+				OPTION_PROGRESS,
+				OPTION_NONE,*/
+		case OPTION_OUT:
+			
+		break;
+		case OPTION_MAIN:
+			SAFE_RELEASE(_currentOption);
+			_currentOption = new option;
+			_currentOption->init();
+		break;
+		case OPTION_GAME:
+			SAFE_RELEASE(_currentOption);
+			_currentOption = new option_game;
+			_currentOption->init();
+		break;
+		case OPTION_CONTROLS:
+			SAFE_RELEASE(_currentOption);
+			_currentOption = new option_controls;
+			_currentOption->init();
+		break;
+		case OPTION_AUDIO:
+			SAFE_RELEASE(_currentOption);
+			_currentOption = new option_audio;
+			_currentOption->init();
+		break;
+		case OPTION_VIDEO:
+			SAFE_RELEASE(_currentOption);
+			_currentOption = new option_video;
+			_currentOption->init();
+		break;
+		case OPTION_SELECT:
+		break;
+		case OPTION_PROGRESS:
+		break;
+		case OPTION_NONE:
+			
+		break;
+		}
 	}
 }
 
