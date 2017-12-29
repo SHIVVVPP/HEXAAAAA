efe4a88b5764132762a8b874b4df31e3318c52df
신우 김 <sinsdhk@gmail.com>
2017-12-29 17:25:19 +0900

    dataBase추가

---
+++ 팀포폴/database.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "singletonBase.h"
+#include <vector>
+#include <map>
+
+
+
+class database : public singletonBase<database>
+{
+public:
+
+
+public:
+	database();
+	~database();
+};
+
