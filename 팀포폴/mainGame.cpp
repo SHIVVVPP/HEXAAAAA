668d80b18a507a5d26ee9ea2c443b3775ee4e662
성위현 <songwehyun@hotmail.com>
2018-01-02 17:11:21 +0900

    인벤 커밋

--- 팀포폴/mainGame.cpp
+++ 팀포폴/mainGame.cpp
@@ -25,10 +25,10 @@ HRESULT mainGame::init()			//초기화 함수
 	gameNode::init(true);
 	setImages();
 
-	/*
+	
 
 	_player = new player;
-	_player->init();*/
+	//_player->init();
 
 	sys = new system_option;
 	sys->init();
@@ -36,7 +36,7 @@ HRESULT mainGame::init()			//초기화 함수
 	SCENEMANAGER->addScene("스테이지", new stage);
 	SCENEMANAGER->addScene("인벤토리", new inventory);
 	
-	SCENEMANAGER->changeScene("인벤토리");
+	SCENEMANAGER->changeScene("인벤토리",_player);
 
 	//_ui = new ui;
 	//_ui->init(UI_STAGE);
@@ -54,7 +54,7 @@ void mainGame::update()				//연산 함수
 {
 	gameNode::update();
 	//sys->update();
-	//SCENEMANAGER->update();
+	SCENEMANAGER->update();
 	//_player->update();
 	
 	//_ui->update();
@@ -66,7 +66,7 @@ void mainGame::render()		//그려주는 함수(a.k.a WM_PAINT)
 	PatBlt(getMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
 	//==================== 건들지마라 ======================
 	//sys->render();
-	//SCENEMANAGER->render();
+	SCENEMANAGER->render();
 
 	//_player->render();
 
